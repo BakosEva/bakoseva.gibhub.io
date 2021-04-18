36795c9d0022918fcaafe9612115ad8a0e10b3b0
BakosEva <bakos.evi@gmail.com>
2021-04-18 22:40:02 +0200

    kapcsolat hozzáadva, javítások

--- js/js.js
+++ js/js.js
@@ -1,7 +1,16 @@
-  $(document).ready(function() {
-          
-       AOS.init();
+  $(document).ready(function () {
+
+      AOS.init();
       
-      
-      
+      $("#datepicker").datepicker({
+          dateFormat: "yy-mm-dd",
+          firstDay: 1,
+          dayNamesMin: ["Va", "Hé", "Ke", "Sze", "Csü", "Pé", "Szo"],
+          monthNames: ["Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December"],
+          minDate: 7,
+          showMonthAfterYear: true,
+          prevText: "Előző",
+          nextText: "Következő",
+      });
+
   });
